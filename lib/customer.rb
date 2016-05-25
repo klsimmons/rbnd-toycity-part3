b3e7970dd8a6a8ed105b8ee118dec0f51b874463
KLSimmons <klsimmons88@gmail.com>
2016-05-25 13:58:19 -0400

    Add Customer class with ability to count customers and find customers by name

---
+++ lib/customer.rb
@@ -0,0 +1,32 @@
+class Customer
+  attr_reader :name
+
+  @@customers = []
+
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(name)
+    @@customers.each do |customer|
+      if customer.name == name
+        return customer
+      end
+    end
+  end
+
+  private
+
+  def add_to_customers
+    if @@customers.map { |customer| customer.name }.include?(@name)
+      raise DuplicateCustomerError, "'#{@name}' already exists."
+    else
+      @@customers << self
+    end
+  end
+end
